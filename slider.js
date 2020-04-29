83a51fd0906ce0f0acdb4b6b80d81457acaff986
Maja kepler <maja@maja2.local>
2020-04-29 20:55:46 +0200

    commit

--- slider.js
+++ slider.js
@@ -2,7 +2,10 @@ let sliderImages = document.querySelectorAll(".slide"),
     arrowLeft = document.querySelector("#arrow_left"),
     arrowRight = document.querySelector("#arrow_right"),
     current = 0;
+//current repræsentere hvilket billede/slide man er på om som standart er den på 0. Da det første billede/silde bliver kaldt 0 i rækkefølgen af billeder
 
+
+//Jeg starter med at lave en reset funktion, fordi hver gang vi ser et nyt billede vil vi fjerne alle de andre billeder.
 function reset() {
     for (let i = 0; i < sliderImages.length; i++) {
         sliderImages[i].style.display = "none";
@@ -13,8 +16,9 @@ function reset() {
 function startSlide() {
     reset();
     sliderImages[0].style.display = "block";
-
 }
+
+
 
 //show prev
 function slideLeft() {
@@ -22,6 +26,8 @@ function slideLeft() {
     sliderImages[current - 1].style.display = "block"
     current--;
 }
+//I denne finktion for vi den venstre pil til at ”virke” ved at når man trykker på pilen skal den kigge på sliderimage og ud fra dens current iteruratens skal den fjerne 1 og så vise det billede i stedet, hvilke gør at current nu skal formindskes med 1 så derfor skriver jeg current --
+
 
 // show next
 function slideRight() {
@@ -29,6 +35,7 @@ function slideRight() {
     sliderImages[current + 1].style.display = "block"
     current++;
 }
+//Vi gør de sammen I denne funkion som i den sidste, her skal vi dog bar forstørre den med en da vi vil gå frem i billederne.
 
 
 //Left arrow click
@@ -38,6 +45,9 @@ arrowLeft.addEventListener("click", function () {
     }
     slideLeft();
 });
+//Vi sætter en if sætning som lyder på at hvis current er lig med 0, så vil vi sætter currenten til at være lig med sliderimage.length. som er det totale nummer af billeder i slideren, hvilket får det til at gå til det sidste billede.
+//    slideLeft(); her kalder vi på funktionen slideleft og vi har lige ovenover sat current til et være 3 med sliderimage.length som nu er 4, men jeg vil gerne have at den trækker en fra, da vores tabel starter med 0 så derfor hedder det sidste billet entlig 3, dette gør den i funktionen slideLeft, hvor vi jo minusser currenten med 1
+
 
 //Right arrow click
 arrowRight.addEventListener("click", function () {
@@ -46,3 +56,5 @@ arrowRight.addEventListener("click", function () {
     }
     slideRight();
 });
+//Denne funktion er meget lig den over men her vil vi gerne gå fra af.
+//Derfor laver vi en if sætning så hvis current er lig med sliderimage.length – 1 altså 3, så hvis det er sandt vil nu have currenten til at være lig med -1, for at når den går vider til funktionen slideRight så ligger den jo 1 til current, så den på den måde bliver 0 og derfor går til det førtste billede i rækken.
